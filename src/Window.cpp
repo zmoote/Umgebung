80062d4f70c00c46365a5f00a3938bb3af3eebe0
Zachary Moote <nuluumo@gmail.com>
2025-05-03 21:26:25 -0400

    Refactor: Remove Vulkan and unused dependencies
    This commit removes Vulkan-related code, including the `VulkanRenderer` class, and cleans up unused dependencies such as Boost, Eigen3, and `nlohmann_json`. Corresponding `find_package` and `target_link_libraries` calls were deleted from `CMakeLists.txt`. The `vcpkg` submodule was updated, and `vcpkg.json` was modified to reflect the removal of unused libraries. These changes simplify the project and align it with updated requirements.

--- src/Window.cpp
+++
@@ -1,6 +0,0 @@
-#include "../include/Window.hpp"
-
-namespace Umgebung 
-{
-
-}
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,3 @@
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
-
-#include <stdexcept>
-#include <vector>
 #include <iostream>
 
 int main()
