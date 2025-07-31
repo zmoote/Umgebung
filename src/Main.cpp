457f7cdbd0a36cf7adb9bbd3473331c73a78adf8
Zachary Moote <nuluumo@gmail.com>
2025-07-31 14:39:50 -0400

    Refactor project structure and update dependencies
    - Updated `CMakeLists.txt` for dynamic CRT linkage and CUDA extensions.
    - Modified `CMakePresets.json` to include vcpkg target triplet.
    - Streamlined logging in `Logger.h`.
    - Updated `vcpkg.json` to add support for the Irrlicht engine and other dependencies.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,372 +1,198 @@
-//
-// Main.cpp
-//
+/** Example 002 Quake3Map
 
-#include "../include/walbourn/pch.h"
-#include "../include/umgebung/Simulation.h"
+This Tutorial shows how to load a Quake 3 map into the engine, create a
+SceneNode for optimizing the speed of rendering, and how to create a user
+controlled camera.
 
-// Forward declare message handler from imgui_impl_win32.cpp
-extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+Please note that you should know the basics of the engine before starting this
+tutorial. Just take a short look at the first tutorial, if you haven't done
+this yet: http://irrlicht.sourceforge.net/tut001.html
 
-using namespace DirectX;
+Lets start like the HelloWorld example: We include the irrlicht header files
+and an additional file to be able to ask the user for a driver type using the
+console.
+*/
+#include <irrlicht.h>
+#include <iostream>
 
-#ifdef __clang__
-#pragma clang diagnostic ignored "-Wcovered-switch-default"
-#pragma clang diagnostic ignored "-Wswitch-enum"
+/*
+As already written in the HelloWorld example, in the Irrlicht Engine everything
+can be found in the namespace 'irr'. To get rid of the irr:: in front of the
+name of every class, we tell the compiler that we use that namespace from now
+on, and we will not have to write that 'irr::'. There are 5 other sub
+namespaces 'core', 'scene', 'video', 'io' and 'gui'. Unlike in the HelloWorld
+example, we do not call 'using namespace' for these 5 other namespaces, because
+in this way you will see what can be found in which namespace. But if you like,
+you can also include the namespaces like in the previous example.
+*/
+using namespace irr;
+
+/*
+Again, to be able to use the Irrlicht.DLL file, we need to link with the
+Irrlicht.lib. We could set this option in the project settings, but to make it
+easy, we use a pragma comment lib:
+*/
+#ifdef _MSC_VER
+#pragma comment(lib, "Irrlicht.lib")
 #endif
 
-#pragma warning(disable : 4061)
-
-extern "C"
+/*
+Ok, lets start. Again, we use the main() method as start, not the WinMain().
+*/
+int main()
 {
-    // Used to enable the "Agility SDK" components
-    __declspec(dllexport) extern const UINT D3D12SDKVersion = D3D12_SDK_VERSION;
-    __declspec(dllexport) extern const char* D3D12SDKPath = ".\\D3D12\\";
+	/*
+	Like in the HelloWorld example, we create an IrrlichtDevice with
+	createDevice(). The difference now is that we ask the user to select
+	which video driver to use. The Software device might be
+	too slow to draw a huge Quake 3 map, but just for the fun of it, we make
+	this decision possible, too.
+	Instead of copying this whole code into your app, you can simply include
+	driverChoice.h from Irrlicht's include directory. The function
+	driverChoiceConsole does exactly the same.
+	*/
+
+	// create device and exit if creation failed
+
+	IrrlichtDevice* device =
+		createDevice(video::EDT_OPENGL, core::dimension2d<u32>(1920, 1080));
+
+	if (device == 0)
+		return 1; // could not create selected driver.
+
+	/*
+	Get a pointer to the video driver and the SceneManager so that
+	we do not always have to call irr::IrrlichtDevice::getVideoDriver() and
+	irr::IrrlichtDevice::getSceneManager().
+	*/
+	video::IVideoDriver* driver = device->getVideoDriver();
+	scene::ISceneManager* smgr = device->getSceneManager();
+
+	/*
+	To display the Quake 3 map, we first need to load it. Quake 3 maps
+	are packed into .pk3 files which are nothing else than .zip files.
+	So we add the .pk3 file to our irr::io::IFileSystem. After it was added,
+	we are able to read from the files in that archive as if they are
+	directly stored on the disk.
+	*/
+	device->getFileSystem()->addFileArchive("media/map-20kdm2.pk3");
+
+	/*
+	Now we can load the mesh by calling
+	irr::scene::ISceneManager::getMesh(). We get a pointer returned to an
+	irr::scene::IAnimatedMesh. As you might know, Quake 3 maps are not
+	really animated, they are only a huge chunk of static geometry with
+	some materials attached. Hence the IAnimatedMesh consists of only one
+	frame, so we get the "first frame" of the "animation", which is our
+	quake level and create an Octree scene node with it, using
+	irr::scene::ISceneManager::addOctreeSceneNode().
+	The Octree optimizes the scene a little bit, trying to draw only geometry
+	which is currently visible. An alternative to the Octree would be a
+	irr::scene::IMeshSceneNode, which would always draw the complete
+	geometry of the mesh, without optimization. Try it: Use
+	irr::scene::ISceneManager::addMeshSceneNode() instead of
+	addOctreeSceneNode() and compare the primitives drawn by the video
+	driver. (There is a irr::video::IVideoDriver::getPrimitiveCountDrawn()
+	method in the irr::video::IVideoDriver class). Note that this
+	optimization with the Octree is only useful when drawing huge meshes
+	consisting of lots of geometry.
+	*/
+	scene::IAnimatedMesh* mesh = smgr->getMesh("media/20kdm2.bsp");
+	scene::ISceneNode* node = 0;
+
+	if (mesh)
+		node = smgr->addOctreeSceneNode(mesh->getMesh(0), 0, -1, 1024);
+	//		node = smgr->addMeshSceneNode(mesh->getMesh(0));
+
+		/*
+		Because the level was not modelled around the origin (0,0,0), we
+		translate the whole level a little bit. This is done on
+		irr::scene::ISceneNode level using the methods
+		irr::scene::ISceneNode::setPosition() (in this case),
+		irr::scene::ISceneNode::setRotation(), and
+		irr::scene::ISceneNode::setScale().
+		*/
+	if (node)
+		node->setPosition(core::vector3df(-1300, -144, -1249));
+
+	/*
+	Now we only need a camera to look at the Quake 3 map.
+	We want to create a user controlled camera. There are some
+	cameras available in the Irrlicht engine. For example the
+	MayaCamera which can be controlled like the camera in Maya:
+	Rotate with left mouse button pressed, Zoom with both buttons pressed,
+	translate with right mouse button pressed. This could be created with
+	irr::scene::ISceneManager::addCameraSceneNodeMaya(). But for this
+	example, we want to create a camera which behaves like the ones in
+	first person shooter games (FPS) and hence use
+	irr::scene::ISceneManager::addCameraSceneNodeFPS().
+	*/
+
+
+	SKeyMap keyMap[4];
+	keyMap[0].Action = EKA_MOVE_FORWARD;
+	keyMap[0].KeyCode = KEY_KEY_W;
+
+	keyMap[1].Action = EKA_MOVE_BACKWARD;
+	keyMap[1].KeyCode = KEY_KEY_S;
+
+	keyMap[2].Action = EKA_STRAFE_LEFT;
+	keyMap[2].KeyCode = KEY_KEY_A;
+
+	keyMap[3].Action = EKA_STRAFE_RIGHT;
+	keyMap[3].KeyCode = KEY_KEY_D;
+
+	smgr->addCameraSceneNodeFPS(0, 100, 0.5f, -1, keyMap, 8);
+
+	/*
+	The mouse cursor needs not be visible, so we hide it via the
+	irr::IrrlichtDevice::ICursorControl.
+	*/
+	device->getCursorControl()->setVisible(false);
+
+	/*
+	We have done everything, so lets draw it. We also write the current
+	frames per second and the primitives drawn into the caption of the
+	window. The test for irr::IrrlichtDevice::isWindowActive() is optional,
+	but prevents the engine to grab the mouse cursor after task switching
+	when other programs are active. The call to
+	irr::IrrlichtDevice::yield() will avoid the busy loop to eat up all CPU
+	cycles when the window is not active.
+	*/
+	int lastFPS = -1;
+
+	while (device->run())
+	{
+		if (device->isWindowActive())
+		{
+			driver->beginScene(true, true, video::SColor(255, 200, 200, 200));
+			smgr->drawAll();
+			driver->endScene();
+
+			int fps = driver->getFPS();
+
+			if (lastFPS != fps)
+			{
+				core::stringw str = L"Irrlicht Engine - Quake 3 Map example [";
+				str += driver->getName();
+				str += "] FPS:";
+				str += fps;
+
+				device->setWindowCaption(str.c_str());
+				lastFPS = fps;
+			}
+		}
+		else
+			device->yield();
+	}
+
+	/*
+	In the end, delete the Irrlicht device.
+	*/
+	device->drop();
+	return 0;
 }
 
-namespace
-{
-    std::unique_ptr<Umgebung::Simulation> g_simulation;
-}
-
-LPCWSTR g_szAppName = L"Umgebung";
-
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-void ExitSimulation() noexcept;
-
-// Entry point
-int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
-{
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
-
-    if (!XMVerifyCPUSupport())
-        return 1;
-
-#ifdef __MINGW32__
-    if (FAILED(CoInitializeEx(nullptr, COINITBASE_MULTITHREADED)))
-        return 1;
-#else
-    if (FAILED(RoInitialize(RO_INIT_MULTITHREADED)))
-        return 1;
-#endif
-
-    g_simulation = std::make_unique<Umgebung::Simulation>();
-
-    // Register class and create window
-    {
-        // Register class
-        WNDCLASSEXW wcex = {};
-        wcex.cbSize = sizeof(WNDCLASSEXW);
-        wcex.style = CS_HREDRAW | CS_VREDRAW;
-        wcex.lpfnWndProc = WndProc;
-        wcex.hInstance = hInstance;
-        wcex.hIcon = LoadIconW(hInstance, L"IDI_ICON1");
-        wcex.hCursor = LoadCursorW(nullptr, IDC_ARROW);
-        wcex.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1);
-        wcex.lpszClassName = L"UmgebungWindowClass";
-        wcex.hIconSm = LoadIconW(wcex.hInstance, L"IDI_ICON1");
-        if (!RegisterClassExW(&wcex))
-            return 1;
-
-        // Create window
-        int w, h;
-        g_simulation->GetDefaultSize(w, h);
-
-        RECT rc = { 0, 0, static_cast<LONG>(w), static_cast<LONG>(h) };
-
-        AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
-
-        HWND hwnd = CreateWindowExW(0, L"UmgebungWindowClass", g_szAppName, WS_OVERLAPPEDWINDOW,
-            CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top,
-            nullptr, nullptr, hInstance,
-            g_simulation.get());
-        // TODO: Change to CreateWindowExW(WS_EX_TOPMOST, L"UmgebungWindowClass", g_szAppName, WS_POPUP,
-        // to default to fullscreen.
-
-        if (!hwnd)
-            return 1;
-
-        ShowWindow(hwnd, nCmdShow);
-        // TODO: Change nCmdShow to SW_SHOWMAXIMIZED to default to fullscreen.
-
-        GetClientRect(hwnd, &rc);
-
-        g_simulation->Initialize(hwnd, rc.right - rc.left, rc.bottom - rc.top);
-    }
-
-    // Main message loop
-    MSG msg = {};
-    while (WM_QUIT != msg.message)
-    {
-        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-        else
-        {
-            g_simulation->Tick();
-        }
-    }
-
-    g_simulation.reset();
-
-#ifdef __MINGW32__
-    CoUninitialize();
-#else
-    RoUninitialize();
-#endif
-
-    return static_cast<int>(msg.wParam);
-}
-
-// Windows procedure
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    // Handle ImGui messages first
-    if (ImGui_ImplWin32_WndProcHandler(hWnd, message, wParam, lParam))
-        return true;
-
-    static bool s_in_sizemove = false;
-    static bool s_in_suspend = false;
-    static bool s_minimized = false;
-    static bool s_fullscreen = false;
-    // TODO: Set s_fullscreen to true if defaulting to fullscreen.
-
-    auto simulation = reinterpret_cast<Umgebung::Simulation*>(GetWindowLongPtr(hWnd, GWLP_USERDATA));
-
-    switch (message)
-    {
-    case WM_CREATE:
-        if (lParam)
-        {
-            auto params = reinterpret_cast<LPCREATESTRUCTW>(lParam);
-            SetWindowLongPtr(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(params->lpCreateParams));
-        }
-        break;
-
-    case WM_PAINT:
-        if (s_in_sizemove && simulation)
-        {
-            simulation->Tick();
-        }
-        else
-        {
-            PAINTSTRUCT ps;
-            std::ignore = BeginPaint(hWnd, &ps);
-            EndPaint(hWnd, &ps);
-        }
-        break;
-
-    case WM_DISPLAYCHANGE:
-        if (simulation)
-        {
-            simulation->OnDisplayChange();
-        }
-        break;
-
-    case WM_MOVE:
-        if (simulation)
-        {
-            simulation->OnWindowMoved();
-        }
-        break;
-
-    case WM_SIZE:
-        if (wParam == SIZE_MINIMIZED)
-        {
-            if (!s_minimized)
-            {
-                s_minimized = true;
-                if (!s_in_suspend && simulation)
-                    simulation->OnSuspending();
-                s_in_suspend = true;
-            }
-        }
-        else if (s_minimized)
-        {
-            s_minimized = false;
-            if (s_in_suspend && simulation)
-                simulation->OnResuming();
-            s_in_suspend = false;
-        }
-        else if (!s_in_sizemove && simulation)
-        {
-            simulation->OnWindowSizeChanged(LOWORD(lParam), HIWORD(lParam));
-        }
-        break;
-
-    case WM_ENTERSIZEMOVE:
-        s_in_sizemove = true;
-        break;
-
-    case WM_EXITSIZEMOVE:
-        s_in_sizemove = false;
-        if (simulation)
-        {
-            RECT rc;
-            GetClientRect(hWnd, &rc);
-
-            simulation->OnWindowSizeChanged(rc.right - rc.left, rc.bottom - rc.top);
-        }
-        break;
-
-    case WM_GETMINMAXINFO:
-        if (lParam)
-        {
-            auto info = reinterpret_cast<MINMAXINFO*>(lParam);
-            info->ptMinTrackSize.x = 320;
-            info->ptMinTrackSize.y = 200;
-        }
-        break;
-
-    case WM_ACTIVATEAPP:
-        if (simulation)
-        {
-            if (wParam)
-            {
-                simulation->OnActivated();
-            }
-            else
-            {
-                simulation->OnDeactivated();
-            }
-        }
-        break;
-
-    case WM_POWERBROADCAST:
-        switch (wParam)
-        {
-        case PBT_APMQUERYSUSPEND:
-            if (!s_in_suspend && simulation)
-                simulation->OnSuspending();
-            s_in_suspend = true;
-            return TRUE;
-
-        case PBT_APMRESUMESUSPEND:
-            if (!s_minimized)
-            {
-                if (s_in_suspend && simulation)
-                    simulation->OnResuming();
-                s_in_suspend = false;
-            }
-            return TRUE;
-
-        default:
-            break;
-        }
-        break;
-
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-
-    case WM_SYSKEYDOWN:
-        if (wParam == VK_RETURN && (lParam & 0x60000000) == 0x20000000)
-        {
-            // Implements the classic ALT+ENTER fullscreen toggle
-            if (s_fullscreen)
-            {
-                SetWindowLongPtr(hWnd, GWL_STYLE, WS_OVERLAPPEDWINDOW);
-                SetWindowLongPtr(hWnd, GWL_EXSTYLE, 0);
-
-                int width = 800;
-                int height = 600;
-                if (simulation)
-                    simulation->GetDefaultSize(width, height);
-
-                ShowWindow(hWnd, SW_SHOWNORMAL);
-
-                SetWindowPos(hWnd, HWND_TOP, 0, 0, width, height, SWP_NOMOVE | SWP_NOZORDER | SWP_FRAMECHANGED);
-            }
-            else
-            {
-                SetWindowLongPtr(hWnd, GWL_STYLE, WS_POPUP);
-                SetWindowLongPtr(hWnd, GWL_EXSTYLE, WS_EX_TOPMOST);
-
-                SetWindowPos(hWnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
-
-                ShowWindow(hWnd, SW_SHOWMAXIMIZED);
-            }
-
-            s_fullscreen = !s_fullscreen;
-        }
-        break;
-
-    case WM_KEYDOWN:
-    case WM_KEYUP:
-    case WM_SYSKEYUP:
-        // Handle additional keyboard shortcuts for your simulation
-        if (message == WM_KEYDOWN)
-        {
-            switch (wParam)
-            {
-            case VK_ESCAPE:
-                // ESC to exit fullscreen or close application
-                if (s_fullscreen)
-                {
-                    // Exit fullscreen
-                    PostMessage(hWnd, WM_SYSKEYDOWN, VK_RETURN, 0x20000000);
-                }
-                else
-                {
-                    // Close application
-                    ExitSimulation();
-                }
-                break;
-
-            case VK_F1:
-                // F1 for help/about (could show ImGui help window)
-                // TODO: Show help window
-                break;
-
-            case VK_F11:
-                // F11 alternative fullscreen toggle
-                PostMessage(hWnd, WM_SYSKEYDOWN, VK_RETURN, 0x20000000);
-                break;
-
-            case '2':
-                if (GetKeyState(VK_CONTROL) & 0x8000)
-                {
-                    // Ctrl+2 for 2D view
-                    // TODO: Switch to 2D multiverse view
-                }
-                break;
-
-            case '3':
-                if (GetKeyState(VK_CONTROL) & 0x8000)
-                {
-                    // Ctrl+3 for 3D view
-                    // TODO: Switch to 3D exploration view
-                }
-                break;
-
-            case 'R':
-                if (GetKeyState(VK_CONTROL) & 0x8000)
-                {
-                    // Ctrl+R to reset simulation
-                    // TODO: Reset simulation
-                }
-                break;
-            }
-        }
-        break;
-
-    case WM_MENUCHAR:
-        // A menu is active and the user presses a key that does not correspond
-        // to any mnemonic or accelerator key. Ignore so we don't produce an error beep.
-        return MAKELRESULT(0, MNC_CLOSE);
-
-    default:
-        break;
-    }
-
-    return DefWindowProc(hWnd, message, wParam, lParam);
-}
-
-// Exit helper
-void ExitSimulation() noexcept
-{
-    PostQuitMessage(0);
-}
+/*
+That's it. Compile and play around with the program.
+**/
