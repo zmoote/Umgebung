a6269947b0855f1cb79b95bbe2d15b7312c1a1e8
Zachary Moote <nuluumo@gmail.com>
2025-08-12 07:38:46 -0400

    Added more research resources

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,11 +5,11 @@ int main(int argc, char** argv)
 {
 
 	irr::IrrlichtDevice* device = irr::createDevice(irr::video::EDT_OPENGL, irr::core::dimension2d<irr::u32>(640, 480));
-	
+
 	if (!device) {
 		return 1;
 	}
-	
+
 	device->maximizeWindow();
 
 	irr::video::IVideoDriver* driver = device->getVideoDriver();
